4dd827d3e32f1e53f622e4e098d693b1c9be8186
eximus <fabio4335@gmail.com>
2024-11-06 19:21:23 -0500

    replace vim with neovim

--- provision/00-custom.sh
+++ provision/00-custom.sh
@@ -2,7 +2,9 @@
 set -eux
 
 DEBIAN_FRONTEND=noninteractive sudo NEEDRESTART_MODE=a apt install -qq -y --no-install-recommends --no-install-suggests \
-    vim-gtk3 lf tmux git make gawk fzf xclip ripgrep bat
+    neovim lf tmux git make gawk fzf xclip ripgrep bat
+
+sudo update-alternatives --set vim /usr/bin/nvim
 
 # install bottom
 curl -sOL https://github.com/ClementTsang/bottom/releases/download/0.9.6/bottom_0.9.6_amd64.deb --output-dir /tmp
